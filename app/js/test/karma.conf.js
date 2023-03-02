e4d0944c5f9d251d9d7b8b0872c5ec2734689613
Beatriz Bravo Gallardo <beatriz.bravo@globant.com>
2023-03-02 15:06:15 -0600

    Testing directive part 2

--- app/js/test/karma.conf.js
+++ app/js/test/karma.conf.js
@@ -4,18 +4,18 @@
 module.exports = function (config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: "",
+    basePath: '',
 
     // frameworks to use
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ["jasmine"],
+    frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
-      "../../bower_components/angular/angular.js",
-      "../../bower_components/angular-mocks/angular-mocks.js",
-      "../app.js",
-      "unit/*.js",
+      '../../bower_components/angular/angular.js',
+      '../../bower_components/angular-mocks/angular-mocks.js',
+      '../app.js',
+      'unit/*.js',
     ],
 
     // list of files / patterns to exclude
@@ -23,12 +23,12 @@ module.exports = function (config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
-    preprocessors: {},
+    preprocessors: { '../../*.html': ['ng-html2js'] },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ["progress"],
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
@@ -45,7 +45,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ["PhantomJS"],
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
